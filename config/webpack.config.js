20c3ed1d76917383f76ccd34573cdbe925e824ef
acedynamicstrading <acedynamicstrading@gmail.com>
2026-08-21 18:08:15 +0530

    Update output filename to include content hash

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, '../dist'),
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash].js',
     publicPath: './',
   },
   devServer: {
